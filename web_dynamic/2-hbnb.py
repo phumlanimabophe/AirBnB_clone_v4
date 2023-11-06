de04eb805b9c450235a8471bbea91afcbc4e38a9
Phumlani Mabophe <93977501+phumlanimabophe@users.noreply.github.com>
2023-11-06 03:25:37 +0000

    some changes

---
+++ web_dynamic/2-hbnb.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+from flask import Flask, render_template, url_for
+from models import storage
+import uuid
+
+# Flask setup
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+server_host = '0.0.0.0'
+
+# Teardown app context
+@app.teardown_appcontext
+def teardown_database_session(exception):
+    """
+    After each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+# Route for rendering the HTML page
+@app.route('/2-hbnb/')
+def render_hbnb_page(the_id=None):
+    """
+    Handles requests to a custom template with states, cities, and amenities
+    """
+    state_objects = storage.all('State').values()
+    states_data = dict([state.name, state] for state in state_objects)
+    amenities_data = storage.all('Amenity').values()
+    places_data = storage.all('Place').values()
+    users_data = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                      for user in storage.all('User').values())
+    cache_id = uuid.uuid4()
+    return render_template('2-hbnb.html',
+                           states=states_data,
+                           amens=amenities_data,
+                           places=places_data,
+                           users=users_data,
+                           cache_id=cache_id)
+
+if __name__ == "__main__":
+    """
+    Main Flask App
+    """
+    app.run(host=server_host, port=port, threaded=True)
---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Flask App that integrates with AirBnB static HTML Template
+"""
+from flask import Flask, render_template, url_for
+from models import storage
+
+# Flask setup
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+port = 5000
+server_host = '0.0.0.0'
+
+# Teardown app context
+@app.teardown_appcontext
+def teardown_database_session(exception):
+    """
+    After each request, this method calls .close() (i.e. .remove()) on
+    the current SQLAlchemy Session
+    """
+    storage.close()
+
+# Route for rendering the HTML page
+@app.route('/hbnb')
+def render_hbnb_page(the_id=None):
+    """
+    Handles requests to a custom template with states, cities, and amenities
+    """
+    state_objects = storage.all('State').values()
+    states_data = dict([state.name, state] for state in state_objects)
+    amenities_data = storage.all('Amenity').values()
+    places_data = storage.all('Place').values()
+    users_data = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
+                      for user in storage.all('User').values())
+    return render_template('100-hbnb.html',
+                           states=states_data,
+                           amens=amenities_data,
+                           places=places_data,
+                           users=users_data)
+
+if __name__ == "__main__":
+    """
+    Main Flask App
+    """
+    app.run(host=server_host, port=port)
